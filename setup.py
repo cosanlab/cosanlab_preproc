f37cf0d9d241aa30a72df2f7521a11a48b4261d3
Luke Chang <lukejchang@gmail.com>
2016-12-14 08:35:54 -0500

    cleaned up setup.py

--- setup.py
+++ setup.py
@@ -12,10 +12,8 @@ setup(
     maintainer='Luke Chang',
     maintainer_email='luke.j.chang@dartmouth.edu',
     url='http://github.com/cosanlab/cosanlab_preproc',
-    install_requires=['nibabel','nipype','numpy', 'pandas', 'matplotlib', 'seaborn'],
+    install_requires=['nibabel','nipype','numpy', 'pandas', 'nltools','matplotlib', 'seaborn'],
     packages=find_packages(exclude=['cosanlab_preproc/tests']),
     license='MIT',
-    # download_url='https://github.com/ljchang/emotionCF/archive/%s.tar.gz' %
-    # __version__,
     **extra_setuptools_args
 )
