91bf4b7d35c40df81634cd84d3648927d60e68a2
ejolly <eshin.jolly@gmail.com>
2018-04-17 14:40:47 -0400

    Fixed imports.

--- cosanlab_preproc/__init__.py
+++ cosanlab_preproc/__init__.py
@@ -2,8 +2,11 @@ __all__ = [
 'interfaces',
 'pipelines',
 'utils',
+'wfmaker'
 '__version__'
 ]
 
 from .pipelines import Couple_Preproc_Pipeline, TV_Preproc_Pipeline
 from .interfaces import Plot_Coregistration_Montage, Plot_Realignment_Parameters, Create_Covariates, Down_Sample_Precision, Filter_In_Mask, Create_Encoding_File
+from .wfmaker import wfmaker
+from .version import __version__
