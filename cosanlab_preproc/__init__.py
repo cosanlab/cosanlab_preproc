17d2f2e7346c91c5916693b5cb74a1508883e1f1
ejolly <eshin.jolly@gmail.com>
2018-12-06 23:17:42 -0500

    Remove unecessary top level imports. Version bump.

--- cosanlab_preproc/__init__.py
+++ cosanlab_preproc/__init__.py
@@ -7,6 +7,5 @@ __all__ = [
 ]
 
 from .pipelines import Couple_Preproc_Pipeline, TV_Preproc_Pipeline
-from .interfaces import Plot_Coregistration_Montage, Plot_Realignment_Parameters, Create_Covariates, Down_Sample_Precision, Filter_In_Mask, Create_Encoding_File
 from .wfmaker import wfmaker
 from .version import __version__
